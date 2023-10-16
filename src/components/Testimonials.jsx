ee0a0a99457bda551d13d4d4c40416766bc668f3
sadafa asfafas <patrisialorenaschone@gmail.com>
2023-10-16 12:38:48 +0200

    the new one

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Testimonials() {
+  return (
+    <div className='testimonialSection'>
+      <div className="testimonialCardContainer">
+        <div className="testimonialCard"></div>
+        <div className="testimonialCard"></div>
+        <div className="testimonialCard"></div>
+        <div className="testimonialCard"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Testimonials
